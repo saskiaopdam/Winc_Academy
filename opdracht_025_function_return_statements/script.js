d462fff6eb602e99e8ad0916a1f09a5e9febb176
Saskia Opdam <opdamsaskia@icloud.com>
2021-04-12 17:05:07 +0200

    Add files via upload

---
+++ opdracht_025_function_return_statements/script.js
@@ -0,0 +1,28 @@
+// Checking if a number is big
+// Kom er niet uit
+const bigNumberCheck = function (number) {
+  const numberBiggerThanHundred = true;
+  if (numberBiggerThanHundred) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+};
+
+// Bouncer at a club
+// Kom er niet uit
+const BouncerBot = function () {
+  if (BouncerBot) console.log("come in");
+  console.log("It's too busy now, come back later");
+  console.log();
+};
+
+// Calculating the average
+
+const average = function (number1, number2, number3, number4, number5) {
+  const average = (number1 + number2 + number3 + number4 + number5) / 5;
+  return average;
+};
+
+const result = average(1, 2, 3, 4, 5);
+console.log(result);
