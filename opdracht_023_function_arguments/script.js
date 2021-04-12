60003c93f02af15f9ffa9dc2b9b5b3c4e7b1b0d5
Saskia Opdam <opdamsaskia@icloud.com>
2021-04-12 11:37:28 +0200

    Delete opdracht_023_function_arguments directory

--- opdracht_023_function_arguments/script.js
+++
@@ -1,7 +0,0 @@
-const paintWall = function (color, wallLocation) {
-  console.log("The " + wallLocation + " wall has been painted " + color);
-};
-
-paintWall("red");
-paintWall("green");
-paintWall("blue", "north");
