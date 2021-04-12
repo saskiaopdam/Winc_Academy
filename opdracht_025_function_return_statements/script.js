ce7ae0b9019caa65b01045f4e5577d526ab48b4a
Saskia Opdam <opdamsaskia@icloud.com>
2021-04-12 17:44:17 +0200

    Add files via upload

--- opdracht_025_function_return_statements/script.js
+++ opdracht_025_function_return_statements/script.js
@@ -8,6 +8,20 @@ const bigNumberCheck = function (number) {
     console.log("false");
   }
 };
+// Oplossing
+// Functie produceert iets, gebruik geen console.log
+const numberIsBig = function (number) {
+  if (number > 100) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(numberIsBig(150));
+console.log(numberIsBig(50));
+console.log(numberIsBig(0));
+console.log(numberIsBig(999));
 
 // Bouncer at a club
 // Kom er niet uit
@@ -16,6 +30,26 @@ const BouncerBot = function () {
   console.log("It's too busy now, come back later");
   console.log();
 };
+
+// Oplossing
+const getBouncerReaction = function (
+  maxVisitors,
+  currentVisitors,
+  ageOfPotentialVisitor
+) {
+  if (ageOfPotentialVisitor < 18) {
+    return "this is a club for adults";
+  }
+  if (currentVisitors >= maxVisitors) {
+    return "it's too busy now, come back later";
+  } else {
+    return "come in";
+  }
+};
+
+console.log(getBouncerReaction(2000, 0, 15)); //"this is a club for adults"
+console.log(getBouncerReaction(2000, 1999, 50)); //"come in"
+console.log(getBouncerReaction(2000, 2000, 40)); //"it's too busy now, come back later"
 
 // Calculating the average
 
