16f8655c3bdac5c0ce44449f4d9c8d21eef65614
Saskia Opdam <opdamsaskia@icloud.com>
2021-04-23 17:11:57 +0200

    Add files via upload

--- opdracht_027_functions_calling_functions/script.js
+++ opdracht_027_functions_calling_functions/script.js
@@ -1,3 +1,5 @@
+// Hey kiddo
+
 const isAdult = function (age) {
   if (age >= 18) {
     return true;
@@ -8,11 +10,20 @@ const isAdult = function (age) {
 
 const greet = function (age) {
   isAdult(age);
-  consi;
-  return "Hello there";
-  return "Hey kiddo";
 };
 
-greet(16);
+greet(20);
 
-// VAT exercise 1
+// VAT calculations 1
+
+const calcVatAmount = function (basePrice, vatPercentage) {
+  const vatAmount = (basePrice * vatPercentage) / 100;
+  return vatAmount;
+};
+const calcPriceIncludingVat = function (basePrice, vatPercentage) {
+  const priceIncludingVat = basePrice + vatAmount;
+};
+
+calcPriceIncludingVat(1000, 21);
+
+// VAT calculation 2
