cafb0312d082912a93d8af87a07d860bd12c4e9a
Saskia Opdam <opdamsaskia@icloud.com>
2021-04-23 17:07:15 +0200

    Add files via upload

---
+++ opdracht_027_functions_calling_functions/script.js
@@ -0,0 +1,18 @@
+const isAdult = function (age) {
+  if (age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const greet = function (age) {
+  isAdult(age);
+  consi;
+  return "Hello there";
+  return "Hey kiddo";
+};
+
+greet(16);
+
+// VAT exercise 1
