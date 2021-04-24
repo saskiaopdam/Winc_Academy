ac865fae7fd5cb160c00021f060112c7a4e63a1d
Saskia Opdam <opdamsaskia@icloud.com>
2021-04-24 17:31:11 +0200

    Add files via upload

--- opdracht_030_guess_game/script.js
+++ opdracht_030_guess_game/script.js
@@ -1,38 +1,36 @@
 // //Guess the number
 
-// let askForNumber = function () {
-//   let number = prompt(
-//     "Voer een nummer in van 0 tot 25 om te beginnen met raden..."
-//   );
-// };
-
-// let askForName = function () {
-//   let name = prompt("Welkom! Wat is je naam?");
-//   document.write("Hey " + name);
-//   askForNumber();
-// };
-
-// askForName();
-
-// let askForNumber = function () {
-//   let number = prompt(
-//     "Voer een nummer in van 0 tot 25 om te beginnen met raden..."
-//   );
-// };
-
-// askForNumber();
-
+// gebruiker vragen om naam
 let name = prompt("Welkom! Wat is je naam?");
 alert("Hey " + name);
+
+// gebruiker vragen om getal tussen 0 en 25
 let number = prompt(
-  "Voer een nummer in van 0 tot 25 om te beginnen met raden..."
+  "Voer een getal in tussen 0 en 25 om te beginnen met raden..."
 );
-if (number != Math.random) {
-  alert("Dat is niet correct");
-  let number = prompt("Voer opnieuw een nummer in");
-}
-if (number == Math.random) {
-  alert("Gefeliciteerd");
+alert("Je hebt " + number + " ingevoerd");
+document.write("Je hebt " + number + " ingevoerd");
+
+// browser random getal laten kiezen tussen 0 en 25
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
 }
 
-alert("Dag " + name + ". Tot de volgende keer.");
+// ingevoerd getal vergelijken met random gekozen getal
+let rightNumber = getRandomIntInclusive(0, 25);
+document.write("Het juiste getal is " + rightNumber);
+
+// if else afgekeken van Winc oplossing
+while (number !== rightNumber) {
+  let number = prompt("Fout gegokt, probeer het opnieuw");
+  if (number > rightNumber) {
+    alert("Je gok was te hoog");
+  } else if (number < rightNumber) {
+    alert("Je gok was te laag");
+  } else {
+    alert("Gefeliciteerd, je hebt gewonnen!");
+    alert("Dag " + name + ". Tot de volgende keer.");
+  }
+}
