21c4cc3757b444783f421ebaccfbe848ba28844c
Saskia Opdam <opdamsaskia@icloud.com>
2021-05-01 21:00:53 +0200

    Add files via upload

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,34 +1,25 @@
 // Variables (element selectie)
-const hamburger = document.querySelector(".page-nav__hamburger");
-const menuItemWork = document.querySelector(".page-nav__menu__link-work");
-const menuItemAbout = document.querySelector(".page-nav__menu__link-about");
-const menuItemContact = document.querySelector(".page-nav__menu__link-contact");
-const menu = document.querySelector(".page-nav__menu");
+const hamburger = document.querySelector(".navbar__hamburger");
+const menu = document.querySelector(".navbar__menu");
 
 // Functions
-const openMenu = function () {
-  menu.classList.add("page-nav__menu-opened");
+const showHamburger = () => {
+  hamburger.classList.remove("navbar__hamburger--hidden");
 };
-const hideHamburger = function () {
-  hamburger.classList.add("page-nav__hamburger-hidden");
+const hideHamburger = () => {
+  hamburger.classList.add("navbar__hamburger--hidden");
 };
-const showHamburger = function () {
-  hamburger.classList.remove("page-nav__hamburger-hidden");
+const showMenu = () => {
+  menu.classList.add("navbar__menu--visible");
 };
-const closeMenu = function () {
-  menu.classList.remove("page-nav__menu-opened");
+const hideMenu = () => {
+  menu.classList.remove("navbar__menu--visible");
 };
 
 // Menu klapt uit/in bij klik op hamburger
-hamburger.addEventListener("click", openMenu);
+hamburger.addEventListener("click", showMenu);
 hamburger.addEventListener("click", hideHamburger);
 
 // Menu klapt in bij klik op menu-item
-menuItemWork.addEventListener("click", closeMenu);
-menuItemWork.addEventListener("click", showHamburger);
-
-menuItemAbout.addEventListener("click", closeMenu);
-menuItemAbout.addEventListener("click", showHamburger);
-
-menuItemContact.addEventListener("click", closeMenu);
-menuItemContact.addEventListener("click", showHamburger);
+menu.addEventListener("click", hideMenu);
+menu.addEventListener("click", showHamburger);
