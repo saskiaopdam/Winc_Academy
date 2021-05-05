510b1339253f85f6872580bf3fa4e39ae858ad98
Saskia Opdam <opdamsaskia@icloud.com>
2021-05-05 11:24:14 +0200

    Add files via upload

--- portfolio/script.js
+++ portfolio/script.js
@@ -3,23 +3,29 @@ const hamburger = document.querySelector(".navbar__hamburger");
 const menu = document.querySelector(".navbar__menu");
 
 // Functions
-const showHamburger = () => {
-  hamburger.classList.remove("navbar__hamburger--hidden");
-};
 const hideHamburger = () => {
   hamburger.classList.add("navbar__hamburger--hidden");
 };
-const showMenu = () => {
-  menu.classList.add("navbar__menu--visible");
+const showHamburger = () => {
+  hamburger.classList.remove("navbar__hamburger--hidden");
 };
 const hideMenu = () => {
   menu.classList.remove("navbar__menu--visible");
 };
+const showMenu = () => {
+  menu.classList.add("navbar__menu--visible");
+};
 
-// Menu klapt uit/in bij klik op hamburger
-hamburger.addEventListener("click", showMenu);
-hamburger.addEventListener("click", hideHamburger);
+const onHamburgerClick = function () {
+  hideHamburger();
+  showMenu();
+};
+const onMenuClick = function () {
+  hideMenu();
+  showHamburger();
+};
 
-// Menu klapt in bij klik op menu-item
-menu.addEventListener("click", hideMenu);
-menu.addEventListener("click", showHamburger);
+//Event listeners
+hamburger.addEventListener("click", onHamburgerClick);
+
+menu.addEventListener("click", onMenuClick);
