eadd782a20fae51b8d8a49636a0a453ba339cb9d
Saskia Opdam <opdamsaskia@icloud.com>
2021-07-12 17:34:53 +0200

    Add files via upload

--- module 6 (async javascript & api's)/opdracht_048_grote_arrays/script.js
+++ module 6 (async javascript & api's)/opdracht_048_grote_arrays/script.js
@@ -1,25 +1,30 @@
 const btnLandenlijst = document.getElementById("btnLandenlijst");
 const btnSteenbokvrouwen = document.getElementById("btnSteenbokvrouwen");
-console.log(btnSteenbokvrouwen);
+const result = document.getElementById("result");
 
-const toonLandenlijst = function () {
-  let regions = randomPersonData.map(function (element) {
-    return element.region;
-  });
+const displaySelection = (elements) => {
+  result.innerHTML = "";
+  let sortedElements = elements.sort();
+  console.log(sortedElements);
 
-  let sortedRegions = regions.sort();
-  // console.log(sortedRegions);
-
-  sortedRegions.forEach((element) => {
+  sortedElements.forEach((element) => {
     // console.log(element);
     let newLi = document.createElement("li");
     // console.log(newLi);
-    document.querySelector("#result").appendChild(newLi);
+    result.appendChild(newLi);
     newLi.textContent = element;
   });
 };
 
-const toonSteenbokvrouwen = function () {
+const toonLandenlijst = () => {
+  let elements = randomPersonData.map((element) => {
+    return element.region;
+  });
+  console.log(elements);
+  displaySelection(elements);
+};
+
+const toonSteenbokvrouwen = () => {
   let steenbokvrouwen = randomPersonData.filter((element) => {
     let month = element.birthday.dmy.slice(3, 5);
     let day = element.birthday.dmy.slice(0, 2);
@@ -32,29 +37,14 @@ const toonSteenbokvrouwen = function () {
       );
     }
   });
-  console.log(steenbokvrouwen);
-  // Je krijgt nu 0 items terug, want geen enkel item voldoet aan de criteria. Maar als je in de randomPersonData.js bij neem Lelia Firulescu de datum wijzigt in 22/01/1989 dan wordt zij wel gevonden.
-
-  let persons = steenbokvrouwen.map(function (element) {
+  let elements = steenbokvrouwen.map((element) => {
     // return `${element.name} ${element.surname}`;
     return element.name + " " + element.surname + " " + element.photo;
   });
-  console.log(persons);
-
-  let sortedPersons = persons.sort();
-  console.log(sortedPersons);
-
-  sortedPersons.forEach((element) => {
-    // console.log(element);
-    let newLi = document.createElement("li");
-    // console.log(newLi);
-    document.querySelector("#result").appendChild(newLi);
-    newLi.textContent = element;
-  });
+  console.log(elements);
+  // Je krijgt nu 0 items terug, want geen enkel item voldoet aan de criteria. Maar als je in de randomPersonData.js bij neem Lelia Firulescu de datum wijzigt in 22/01/1989 dan wordt zij wel gevonden.
+  displaySelection(elements);
 };
 
 btnLandenlijst.addEventListener("click", toonLandenlijst);
 btnSteenbokvrouwen.addEventListener("click", toonSteenbokvrouwen);
-
-// element.birthday.dmy.slice(3, 5) === "12") ||
-// element.birthday.dmy.slice(3, 5) === "01"
